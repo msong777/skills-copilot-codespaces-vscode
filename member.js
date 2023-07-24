92290ea28a5912519306bd9ebde07d0ef38a8d6a
msong777 <139721399+msong777@users.noreply.github.com>
2023-07-24 05:19:25 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,32 @@
+function skillsMember()
+{
+    var member = document.getElementById("member").value;
+    var member = member.toLowerCase();
+    var member = member.replace(/\s+/g, '');
+    var member = member.replace(/[^a-zA-Z ]/g, "");
+    var member = member.replace(/\s+/g, '');
+    var member = member.replace(/[^a-zA-Z ]/g, "");
+    var member = member.replace(/\s+/g, '');
+    var member = member.replace(/[^a-zA-Z ]/g, "");
+    var member = member.replace(/\s+/g, '');
+    var member = member.replace(/[^a-zA-Z ]/g, "");
+    var member = member.replace(/\s+/g, '');
+    var member = member.replace(/[^a-zA-Z ]/g, "");
+    var member = member.replace(/\s+/g, '');
+    var member = member.replace(/[^a-zA-Z ]/g, "");
+    var member = member.replace(/\s+/g, '');
+    var member = member.replace(/[^a-zA-Z ]/g, "");
+    var member = member.replace(/\s+/g, '');
+    var member = member.replace(/[^a-zA-Z ]/g, "");
+    var member = member.replace(/\s+/g, '');
+    var member = member.replace(/[^a-zA-Z ]/g, "");
+    var member = member.replace(/\s+/g, '');
+    var member = member.replace(/[^a-zA-Z ]/g, "");
+    var member = member.replace(/\s+/g, '');
+    var member = member.replace(/[^a-zA-Z ]/g, "");
+
+    if(member == "member")
+    {
+        document.getElementById("member").value = "";
+    }
+}
